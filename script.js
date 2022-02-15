c7dc177435bf550ffd77bce9eb5b7d54a55ab954
Angelo Curio <iamanauthor9@gmail.com>
2022-02-15 16:56:25 +0800

    Added the container

---
+++ script.js
@@ -0,0 +1,7 @@
+let gridContainer = document.getElementById("grid-container");
+let grids = document.createElement("div");
+gridContainer.appendChild(grids);
+grids.classList.add("grids");
+
+// make a for loop to create the row
+// make another for loop to create the column
